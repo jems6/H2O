e0066cfef35763fcc7fd38839d81e1858e593238
Josue Melendez <jmelend3@gpu-a6-2.zaratan.umd.edu>
2025-05-20 15:41:10 -0400

    H20 changes

--- h2o_hf/utils_lm_eval/modify_llama_tf_4_44.py
+++ h2o_hf/utils_lm_eval/modify_llama_tf_4_44.py
@@ -17,6 +17,101 @@ from transformers.cache_utils import Cache
 from transformers.models.llama.configuration_llama import LlamaConfig
 from transformers.models.llama.modeling_llama import LlamaRotaryEmbedding, LlamaAttention, apply_rotary_pos_emb, repeat_kv
 
+import json
+import logging
+import requests
+import torch
+from typing import Dict, List, Optional
+
+class HuggingFaceModel:
+    def __init__(self, name_or_path: str, attn_implementation: str = "sdpa", topk: int = None, topk_adaptive: int = None, **generation_kwargs) -> None:
+        from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+
+        self.tokenizer = AutoTokenizer.from_pretrained(name_or_path, trust_remote_code=True)
+
+        if 'Yarn-Llama' in name_or_path:
+            model_kwargs = None
+        else:
+            model_kwargs = {"attn_implementation": attn_implementation}
+            if topk is not None:
+                model_kwargs["topk"] = topk
+                self.topk = topk
+            if topk_adaptive is not None:
+                model_kwargs["topk_adaptive"] = topk_adaptive
+                self.topk_adaptive = topk_adaptive
+        
+        # try:
+        #     self.pipeline = pipeline(
+        #         "text-generation",
+        #         model=name_or_path,
+        #         tokenizer=self.tokenizer,
+        #         trust_remote_code=True,
+        #         device_map="auto",
+        #         torch_dtype=torch.bfloat16,
+        #         model_kwargs=model_kwargs,
+        #     )
+        # except:
+        self.pipeline = None
+        self.model = AutoModelForCausalLM.from_pretrained(name_or_path, trust_remote_code=True, device_map="auto", torch_dtype=torch.bfloat16, **model_kwargs)
+            
+        self.generation_kwargs = generation_kwargs
+        self.stop = self.generation_kwargs.pop('stop')
+
+        if self.tokenizer.pad_token is None:
+            # add pad token to allow batching (known issue for llama2)
+            self.tokenizer.padding_side = 'left'
+            self.tokenizer.pad_token = self.tokenizer.eos_token
+            self.tokenizer.pad_token_id = self.tokenizer.eos_token_id
+
+
+    def __call__(self, prompt: str, **kwargs) -> dict:
+        return self.process_batch([prompt], **kwargs)[0]
+
+    def process_batch(self, prompts: List[str], faiss_cache=None, topk_k=None, **kwargs) -> List[dict]:
+        # kv_cache must be of type DynamicFaissCache
+        # if self.pipeline is None:
+        inputs = self.tokenizer(prompts, return_tensors="pt", padding=True).to(self.model.device)
+        if faiss_cache is not None:
+            input_ids = inputs["input_ids"]
+            generated_ids = self.model.generate(
+                input_ids,
+                use_cache=True,
+                past_key_values=faiss_cache,
+                topk_k=topk_k,
+                query_mode=True,
+                **self.generation_kwargs
+            )
+        else: 
+            generated_ids = self.model.generate(
+                **inputs,
+                **self.generation_kwargs
+            )
+        generated_texts = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
+        # else:
+        #     output = self.pipeline(text_inputs=prompts, **self.generation_kwargs, )
+        #     assert len(output) == len(prompts)
+        #     # output in the form of a list of list of dictionaries
+        #     # outer list len = batch size
+        #     # inner list len = 1
+        #     generated_texts = [llm_result[0]["generated_text"] for llm_result in output]
+
+        results = []
+
+        for text, prompt in zip(generated_texts, prompts):
+            # remove the input form the generated text
+            if text.startswith(prompt):
+                text = text[len(prompt):]
+
+            if self.stop is not None:
+                for s in self.stop:
+                    text = text.split(s)[0]
+
+            results.append({'text': [text]})
+
+        return results
+
+
+
 class LlamaAttention_heavy_hitter(nn.Module):
     """Multi-headed attention from 'Attention Is All You Need' paper"""
 
